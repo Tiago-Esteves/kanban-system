621d8da4df0c50a6b6e5b1225214f013a740d3e1
Tiago-Esteves <tiagohk.061098@gmail.com>
2025-09-11 12:57:44 -0300

    alteração cors 2

--- kanban-backend/src/main/java/br/com/tiago/kanban/config/SecurityConfig.java
+++ kanban-backend/src/main/java/br/com/tiago/kanban/config/SecurityConfig.java
@@ -103,6 +103,7 @@ public class SecurityConfig {
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+		configuration.setAllowedOrigins(List.of("https://kanban-system-tau.vercel.app"));
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(List.of("*"));
 		configuration.setAllowCredentials(true);
