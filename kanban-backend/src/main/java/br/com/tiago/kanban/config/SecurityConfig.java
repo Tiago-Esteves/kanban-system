724c6522e02c4e48c23609b5361c0e78af4b0196
Tiago-Esteves <tiagohk.061098@gmail.com>
2025-09-11 13:04:24 -0300

    cors

--- kanban-backend/src/main/java/br/com/tiago/kanban/config/SecurityConfig.java
+++ kanban-backend/src/main/java/br/com/tiago/kanban/config/SecurityConfig.java
@@ -102,8 +102,7 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(List.of("http://localhost:5173"));
-		configuration.setAllowedOrigins(List.of("https://kanban-system-tau.vercel.app"));
+		configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://kanban-system-tau.vercel.app"));
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(List.of("*"));
 		configuration.setAllowCredentials(true);
