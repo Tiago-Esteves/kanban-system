f63d0619590d6fa099834e6caadb8866ef8e1aff
Tiago-Esteves <tiagohk.061098@gmail.com>
2025-09-17 09:45:04 -0300

    alterando hospodegem do backend

--- kanban-backend/src/main/java/br/com/tiago/kanban/model/repositories/TarefaRepository.java
+++ kanban-backend/src/main/java/br/com/tiago/kanban/model/repositories/TarefaRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.tiago.kanban.model.entities.Tarefa;
 
 public interface TarefaRepository extends JpaRepository<Tarefa, Integer>{
-
+	
 }
--- kanban-backend/src/main/java/br/com/tiago/kanban/service/TarefaService.java
+++ kanban-backend/src/main/java/br/com/tiago/kanban/service/TarefaService.java
@@ -3,6 +3,7 @@ package br.com.tiago.kanban.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import br.com.tiago.kanban.model.entities.Coluna;
